aa524ad35e1abe47f497fb0d93ef5306e6ed7e39
DIKSHanW <73245119+DIKSHanW@users.noreply.github.com>
2020-11-15 17:01:41 +0530

    Add files via upload

---
+++ c-78hw/main.js
@@ -0,0 +1,18 @@
+var image = [
+    "me.jpeg",
+    "mother.jpeg",
+    "father.jpeg",
+    "maternal.jpeg",
+    "paternal.jpeg"
+];
+var name = [
+    "Dikshan Pal",
+    "Anjali Singh",
+    "Koushik Pal",
+    "Mala Singh and Ajay Narayan Singh",
+    "Anita Pal and Kali Pada Pal"
+];
+function change(){
+    document.getElementById("images").src = image
+    document.getElementById("names").value = name
+}
